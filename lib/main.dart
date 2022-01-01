b24b878208c82c3d6fa54f8f748e81ecdb793558
RafliAnwar <rafli.anwar2001@gmail.com>
2022-01-01 17:52:52 +0700

    Slicing ChatPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_ecommerce/pages/detail_chat_page.dart';
 import 'package:shamo_ecommerce/pages/home/main_page.dart';
 import 'package:shamo_ecommerce/pages/sign_in_page.dart';
 import 'package:shamo_ecommerce/pages/sign_up_page.dart';
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         '/SignIn': (context) => SignInPage(),
         '/SignUp': (context) => SignUpPage(),
         '/home': (context) => MainPage(),
+        '/detail_chat':(context) =>DetailChatPage(),
       },
     );
   }
--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,10 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_ecommerce/theme.dart';
+import 'package:shamo_ecommerce/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ChatPage'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: bgColor1,
+        centerTitle: true,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Message Support',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+      );
+    }
+
+    Widget emptyChat() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: bgColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icon_headset.png',
+                width: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Opss no message yet?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text('You have never done a transaction',
+                  style: secondaryTextStyle),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                width: 152,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 24,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 16, fontWeight: medium),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: bgColor3,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+            children: [
+              ChatTile(),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+        // emptyChat(),
+      ],
     );
   }
 }
