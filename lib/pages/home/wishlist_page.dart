0b11f2829ce5060859e4958d2dd27de3d1f9a18e
RafliAnwar <rafli.anwar2001@gmail.com>
2022-01-01 23:03:37 +0700

    Slicing Wishlist Page

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -1,10 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_ecommerce/theme.dart';
+import 'package:shamo_ecommerce/widgets/wishlist_card.dart';
 
 class WishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('WishlistPage'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: bgColor1,
+        title: Text(
+          'Favoriet Shoes',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishlist() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: bgColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image_love.png',
+                width: 74,
+                height: 62,
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                ' You don\'t have dream shoes?',
+                style: primaryTextStyle.copyWith(
+                  fontWeight: medium,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Let\'s find your favorite shoes',
+                style: secondaryTextStyle,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 24,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: bgColor3,
+          child: ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: defaultMargin,
+            ),
+            children: [
+              WishlistCard(),
+              WishlistCard(),
+              WishlistCard(),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        // emptyWishlist(),
+        content(),
+      ],
     );
   }
 }
---
+++ lib/widgets/wishlist_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_ecommerce/theme.dart';
+
+class WishlistCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 20,
+      ),
+      padding: EdgeInsets.only(
+        top: 10,
+        left: 12,
+        bottom: 14,
+        right: 20,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgColor4,
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              'assets/image_shoes.png',
+              width: 60,
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Terrex Urban Low',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold,
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  '\$68,47',
+                  style: priceTextStyle,
+                )
+              ],
+            ),
+          ),
+          Image.asset(
+            'assets/button_wishlist_2.png',
+            width: 34,
+          )
+        ],
+      ),
+    );
+  }
+}
